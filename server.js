7dc98a6186d9e3d3885419fbe91599f6b7c6dd06
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-21 20:29:10 -0700

    build API

---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict'
+
+console.log('first server');
+
+require('dotenv').config();
+const weatherData = require('./data/weather.json');
+const express = require('express');
+const app = express();
+// const axios = require('axios');
+// const getWeather = require('./data/weather');
+
+const cors = require('cors');
+// const { response } = require('express');
+
+
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/' , (req,res) => {
+    res.send('Welcome to our server');
+}) 
+
+app.get('/weather' ,(request, response) => {
+
+    let searchQueryCity = request.query.searchQueryCity;
+    let cityWeather = weatherData.find((e) => e.city_name.toLowerCase() === searchQueryCity.toLowerCase());
+    let selectedCity = cityWeather.data.map(dailyWeather => {
+        return new Forcast(dailyWeather);
+    });
+
+    response.send(selectedCity);
+
+})
+
+class Forecast {
+    constructor(cityWeather) {
+      this.date = cityWeather.datetime;
+      this.description = cityWeather.weather.description;
+    }
+}
+
+app.get('*' , (req,res) => {    
+    res.send('Page not found here : error');
+})    
+
+
+    class Forcast {
+        constructor(cityWeather) {
+            this.date = cityWeather.datetime;
+            this.description = cityWeather.weather.description;
+        }
+    }
+
+
+app.use((error, request, response, next) => {
+    response.status(500).send(error.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
